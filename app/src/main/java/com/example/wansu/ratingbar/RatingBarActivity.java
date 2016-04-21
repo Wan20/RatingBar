44d4e78e0d4c9712d433e3f16c711875b8f421ba
Wan20 <wandy20number@gmail.com>
2016-04-21 22:45:30 +0700

    Program

---
+++ app/src/main/java/com/example/wansu/ratingbar/RatingBarActivity.java
@@ -0,0 +1,13 @@
+package com.example.wansu.ratingbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RatingBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating_bar);
+    }
+}
