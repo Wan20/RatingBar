14513b8a2a5bcb69458b61a5af6a4d33d7408193
Wan20 <wandy20number@gmail.com>
2016-04-21 23:28:29 +0700

    Code

--- app/src/main/java/com/example/wansu/ratingbar/RatingBarActivity.java
+++ app/src/main/java/com/example/wansu/ratingbar/RatingBarActivity.java
@@ -2,12 +2,39 @@ package com.example.wansu.ratingbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
-public class RatingBarActivity extends AppCompatActivity {
+public class RatingBarActivity extends AppCompatActivity
+{
+    RatingBar rate;
+    Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating_bar);
+        addListenerOnButtonClick();
+    }
+
+    public void addListenerOnButtonClick()
+    {
+        rate = (RatingBar) findViewById(R.id.ratingbar1);
+        button = (Button) findViewById(R.id.button1);
+
+        //Performing action on Button Click
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Getting the rating and displaying it on the toast
+                String rating = String.valueOf(rate.getRating());
+                Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
